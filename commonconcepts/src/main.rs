140d90a7ef858d1c7d60a578812e682cb283fe63
kodepirate <aniket.das9062@gmail.com>
2024-02-20 18:43:26 +0530

    initial commit

---
+++ commonconcepts/src/main.rs
@@ -0,0 +1,57 @@
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is: {}", x);
+
+//     //Shadowing
+//     let x = "hello";
+//     println!("The value of x is: {}", x);
+
+//     const MAX_POINTS: u32 = 100_000;
+//     let tup = ("hello", 5, 6.4);
+    
+//     let (x, y, z) = tup;
+//     println!("The value of y is: {}", y);
+// }
+
+// fn main() {
+//     // DataTypes: Scalar Types represented by a single value
+//     // Integer Types
+//     //Floating-Point Types
+//     //Boolean Types
+//     //Character Types
+
+//     //Integer Overflow
+//     let  f:u8 = 255;
+
+//     //Compound Types
+//     let tup = (500, 6.4, "hello");
+//     let (x, y, z) = tup; //Destructuring
+//     let poin = tup.2; // Dot notation
+//     println!("The value of poin is: {}", poin);
+
+//     //Array
+//     let a = [1, 2, 3, 4, 5];
+//     let a2 = a[1];
+
+// //creating an empty array
+//     let  a3 = [3; 5]; // [3, 3, 3, 3, 3]
+//     print!("The value of a3 is: {}", a3[1])
+
+// }
+
+
+// fn main() {
+//     //Functions
+//   let sum = my_function(2, 3);
+//     println!("The value of sum is: {}", sum);
+// }
+
+// fn my_function(a: i32, b: i32) -> i32 {
+//     println!("Hello from my_function");// Statements
+//          a +b// Expressions
+          
+// }
+
+fn main(){
+    // Control Flow
+}
