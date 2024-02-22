f9905fdd52247be0c7be1a90da1f903574f9ad98
kodepirate <aniket.das9062@gmail.com>
2024-02-22 20:04:45 +0530

    Structs

--- Structs/src/main.rs
+++ Structs/src/main.rs
@@ -1,3 +1,18 @@
+struct User {
+    username: String,
+    email: String,
+    active_user: bool,
+    signin_count: u64
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User{
+        email: String::from("akash!mail.com"),
+        username: String::from("Akash"),
+        active_user: true,
+        signin_count:1
+    };
+
+    let name  = user1.username;    
 }
