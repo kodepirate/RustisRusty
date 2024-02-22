3bba465679e95fdf025533f2958f40471d02f07d
kodepirate <aniket.das9062@gmail.com>
2024-02-22 21:41:41 +0530

    Structs

--- Structs/src/main.rs
+++ Structs/src/main.rs
@@ -7,12 +7,24 @@ struct User {
 
 
 fn main() {
-    let user1 = User{
+    let mut user1 = User{
         email: String::from("akash!mail.com"),
         username: String::from("Akash"),
         active_user: true,
         signin_count:1
     };
 
-    let name  = user1.username;    
+    let name  = user1.username;    // dot notaion // We can modify our structs using dot notation but we need to make the user to mutable
+    user1.username = String:: from("@gmail.com");
+
+    let user2 = build_user(String::from("fuciik@gmail.com"), String::from("kodeportal"));
+}
+
+fn build_user(email: String, username:String)-> User{
+    User{
+        email, // This is called the field init shorthand
+        username,
+        active_user: true,
+        signin_count:1,
+    }
 }
